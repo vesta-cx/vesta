264ce39c88085de7df560ac2179c05bba8b46b1e
Mia <mia@mia.cx>
2025-05-09 02:57:24 +0200

    ✨ feat(ui): add Link component with todo for internal/external icons
    Introduce a new Link component to handle navigation within the site
    and to external pages. The component displays a right arrow icon for
    internal links and an up-right arrow icon for external links by default.
    
    This improves UX by visually distinguishing between internal and
    external navigation, enhancing clarity for users.

---
+++ packages/ui/src/lib/components/Link.svelte
@@ -0,0 +1,41 @@
+<!-- Link to route on same site: use right arrow icon -->
+<!-- Link to page on other site: use up-right arrow icon -->
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	let {
+		href,
+		icon,
+		children
+	}: {
+		href: string;
+		icon?: Snippet;
+		children: Snippet;
+	} = $props();
+
+	let isExternal = $derived(!href.startsWith('/'));
+
+	const defaultIcons = {
+		internal: 'arrow-right',
+		external: 'arrow-up-right'
+	};
+</script>
+
+{#snippet default_icon()}
+    
+{/snippet}
+
+{#snippet default_icon_external()}
+  
+{/snippet}
+
+<a {href} class="link">
+	{@render children()}
+	<span class="icon">
+		{#if icon}
+			{@render icon()}
+		{:else}
+			<!-- default icons depending on  -->
+		{/if}
+	</span>
+</a>

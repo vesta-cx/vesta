6c4de79cfd50613657104035bcd47e6d73e95f94
Mia <mia@mia.cx>
2025-11-07 13:26:24 +0100

    gitmodules + delete ui

--- packages/ui/src/lib/components/RouteList.svelte
+++
@@ -1,6 +0,0 @@
-<ul>
-    <li><a href="/">Parent route</a></li>
-    <li><a href="/child">Child route</a></li>
-    <li><a href="/child/grandchild">Grandchild route</a></li>
-    <li><a href="/sibling">Sibling route</a></li>
-</ul>

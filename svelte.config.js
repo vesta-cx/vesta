399317168c944f012e7d54de4074c4d321924fc4
Mia <mia@mia.cx>
2025-11-07 13:50:21 +0100

    add shadcn-svelte

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,12 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		alias: {
+			'@': 'src/lib/components',
+		}
+	}
 };
 
 export default config;

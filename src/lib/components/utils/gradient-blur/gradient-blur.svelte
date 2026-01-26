36a672189c665ab81bed0903f2107e84a7ef3a0f
mia-riezebos <mia@mia.cx>
2026-01-26 16:08:49 +0100

    simplify gradient blur & add custom css properties

--- src/lib/components/utils/gradient-blur/gradient-blur.svelte
+++ src/lib/components/utils/gradient-blur/gradient-blur.svelte
@@ -1,67 +1,52 @@
 <script lang="ts">
-	import { stripUnit, type LengthUnit, type Prettify } from '@vesta-cx/utils';
-
-	type BaseMaskOptions = {
-		gradient: 'linear' | 'radial';
-	};
-
-	type Anchor = `top` | `bottom` | `left` | `right`;
-
-	type Position =
-		| `${number}${LengthUnit} ${number}${LengthUnit}`
-		| `at ${Anchor}`
-		| `at ${Anchor} ${Anchor}`;
-
-	type LinearMaskOptions = BaseMaskOptions & {
-		gradient?: 'linear';
-		angle: `${number}deg` | `to ${Anchor}` | `to ${Anchor} ${Anchor}`;
-	};
-
-	type RadialMaskOptions = BaseMaskOptions & {
-		gradient: 'radial';
-		position: Position;
-	};
+	import { stripUnit, type GradientOptions, type LengthUnit } from '@vesta-cx/utils';
+	import { onMount } from 'svelte';
+	import type { HTMLAttributes } from 'svelte/elements';
 
 	type GradientBlurProps = {
 		blur: `${number}${LengthUnit}` | number;
 		detail: number;
-		mask: LinearMaskOptions | RadialMaskOptions;
+		mask: GradientOptions;
 	};
 
 	let {
-		blur = `64px`,
+		blur = `24px`,
 		detail = 8,
-		mask = { gradient: 'linear', angle: 'to top' },
-		class: className = ''
-	}: GradientBlurProps & { class?: string } = $props();
+		mask = { type: 'linear', angle: 'to top' },
+		class: className = '',
+		...restProps
+	}: HTMLAttributes<HTMLDivElement> & GradientBlurProps & { class?: string } = $props();
 
 	// exponentially space blur values from 0 to blur, in steps of {detail}
-	// using the formula: f(x) = 0.5^7 blur 2^(8x)
-
+	// using the formula: f(x) = 0.5^8 blur 2^(8x)
 	const blurValues = Array.from({ length: detail }, (_, i) => {
 		const [number, unit] = typeof blur === 'number' ? [blur, 'px'] : stripUnit(blur);
-		const a = i / detail;
-		return `${0.5 ** 7 * number * 2 ** (8 * a)}${unit}`;
+		const a = i / (detail - 1);
+		return `${0.5 ** 8 * number * 2 ** (8 * a)}${unit}`;
 	});
 
 	const getMaskStops = (i: number) => {
 		const stops: string[] = [];
 		for (let j = 0; j < 4; j++) {
-			const a = (i + j) / detail;
+			const a = (i + j) / (detail + 1);
 			stops.push(`--mask-stop-${j + 1}: ${j == 0 || j == 3 ? 'transparent' : 'black'} ${a * 100}%`);
 		}
 		return stops.join('; ');
 	};
 
-	const getMask = (i: number, mask: LinearMaskOptions | RadialMaskOptions) => {
-		if (mask.gradient === 'linear') {
+	const getMask = (i: number, mask: GradientOptions) => {
+		if (mask.type === 'linear') {
 			return `linear-gradient(${mask.angle}, var(--mask-stop-1), var(--mask-stop-2), var(--mask-stop-3), var(--mask-stop-4))`;
 		}
 		return `radial-gradient(${mask.position}, var(--mask-stop-1), var(--mask-stop-2), var(--mask-stop-3), var(--mask-stop-4))`;
 	};
+
+	onMount(() => {
+		console.log(blurValues);
+	});
 </script>
 
-<div class="absolute z-0 -inset-4 {className}">
+<div class="absolute -inset-4 {className}" {...restProps}>
 	{#each blurValues as blur, i}
 		<div
 			class="
@@ -76,5 +61,30 @@
 	{/each}
 </div>
 
-<style>
+<style lang="scss">
+	
+		@property --top {
+			syntax: '<length-percentage>';
+			initial-value: -1rem;
+			inherits: false;
+		}
+
+		@property --right {
+			syntax: '<length-percentage>';
+			initial-value: -1rem;
+			inherits: false;
+		}
+
+		@property --bottom {
+			syntax: '<length-percentage>';
+			initial-value: -1rem;
+			inherits: false;
+		}
+
+		@property --left {
+			syntax: '<length-percentage>';
+			initial-value: -1rem;
+			inherits: false;
+		}
+	
 </style>

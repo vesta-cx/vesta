01cef62b0ad524a602afad0e46c8b83386384ed1
mia-riezebos <mia@mia.cx>
2026-01-26 18:06:56 +0100

    "polyfill" view-timeline support using intersection-observer, use css implementation on supported browsers.

--- src/lib/components/layout/header/header.svelte
+++ src/lib/components/layout/header/header.svelte
@@ -12,6 +12,7 @@
 	let { children, class: className, ...restProps }: HeaderProps = $props();
 
 	let heroVisible = $state(true);
+	let heroIntersectionRatio = $state(1);
 
 	onMount(() => {
 		// set css var --header-height on :root to the height of the header
@@ -22,12 +23,28 @@
 		}
 
 		if (hero) {
-			const observer = new IntersectionObserver(
-				([entry]) => {
-					heroVisible = entry.isIntersecting;
-				},
-				{ threshold: 0.1 }
-			);
+			const supportsViewTimeline = CSS.supports(
+						'(view-timeline: --hero-timeline) and (animation-timeline: --hero-timeline) and (animation-range: exit 0% 90%) and (timeline-scope: --hero-timeline)'
+					);
+			let observer: IntersectionObserver | null = null;
+
+			if (!supportsViewTimeline) {
+				observer = new IntersectionObserver(
+					([entry]) => {
+						heroIntersectionRatio = 1-(Math.max(entry.intersectionRatio, 0.1) - 0.1) / 9 * 10;
+						header?.style.setProperty('--hero-intersection-ratio', `${(-5 + heroIntersectionRatio * 3) *100}%`);
+						heroVisible = heroIntersectionRatio < 0.9;
+					},
+					{ threshold: Array.from({ length: 1000 }, (_, i) => i / 1000) }
+				);
+			} else {
+				observer = new IntersectionObserver(
+					([entry]) => {
+						heroVisible = entry.isIntersecting;
+					},
+					{ threshold: 0.1 }
+				);
+			}
 
 			observer.observe(hero);
 			return () => observer.disconnect();
@@ -47,14 +64,14 @@
 >
 	<GradientBlur
 		data-section="header-background"
-		class="bottom-(--bottom)"
+		class="bottom-(--hero-intersection-ratio,var(--bottom)) -z-20"
 		blur={12}
 		detail={8}
 		mask={{ type: 'linear', angle: 'to top' }}
 	/>
 
 	<EasedGradient
-		class="absolute inset-0 bottom-(--bottom)"
+		class="absolute inset-0 bottom-(--hero-intersection-ratio,var(--bottom))"
 		data-section="header-background"
 		detail={8}
 		easeFunction={easeOutQuad}
@@ -70,15 +87,15 @@
 			relative z-10 container
 			flex flex-row
 			justify-between gap-6 overflow-clip
-			sm:rounded-2xl rounded-none
-			max-sm:pt-6 p-4 sm:mt-2
-			text-card-foreground
-			transition-[background-color,color,border-width,border-color,box-shadow,margin,padding,width,height,border-radius]
-			duration-300
+			rounded-none p-4
+			text-card-foreground transition-[background-color,color,border-width,border-color,box-shadow,margin,padding,width,height,border-radius] duration-300
 			ease-out
+			max-sm:pt-6
+			sm:mt-2
+			sm:rounded-2xl
 			{!heroVisible
-			? 'sm:mt-4 sm:border border-border bg-card text-card-foreground shadow-sm'
-			: 'sm:border border-border/0'}
+			? 'border-border bg-card/50 text-card-foreground shadow-sm backdrop-blur-sm sm:mt-4 sm:border'
+			: 'border-border/0 sm:border'}
 		"
 	>
 		{#if slots.logo}
@@ -89,7 +106,7 @@
 		{/if}
 		<div class="absolute right-0 bottom-0 left-0 h-px bg-border"></div>
 		<div
-			data-component="scroll-progress"
+			data-section="header-scroll-progress"
 			class="absolute right-(--right) bottom-0 left-0 h-px bg-primary/50"
 		></div>
 	</div>
@@ -99,71 +116,77 @@
 </header>
 
 <style lang="scss">
-	@property --top {
-		syntax: '<length-percentage>';
-		initial-value: 0%;
-		inherits: false;
-	}
-
-	@property --right {
-		syntax: '<length-percentage>';
-		initial-value: 0%;
-		inherits: false;
-	}
-
-	@property --bottom {
-		syntax: '<length-percentage>';
-		initial-value: 0%;
-		inherits: false;
-	}
-
-	@property --left {
-		syntax: '<length-percentage>';
-		initial-value: 0%;
-		inherits: false;
-	}
-
-	@keyframes scroll-progress {
-		from {
-			--right: 100%;
-		}
-		to {
-			--right: 0%;
-		}
-	}
-
-	header [data-component='scroll-progress'] {
-		animation: scroll-progress linear;
-		animation-timeline: scroll();
-	}
-
 	:global {
-		@keyframes header-scroll {
-			from {
-				--bottom: -500%;
-			}
-			to {
-				--bottom: -200%;
-			}
+		header + main section:first-of-type {
+			@apply first:pt-(--header-height);
 		}
+	}
 
+	@supports (view-timeline: --hero-timeline) and (animation-timeline: --hero-timeline) and
+		(animation-range: exit 0% 90%) and (timeline-scope: --hero-timeline) {
 		:root {
 			timeline-scope: --hero-timeline;
 		}
 
-		header + main section:first-of-type {
-			@apply first:pt-(--header-height);
-
-			&[data-section='hero'] {
-				view-timeline-name: --hero-timeline;
-			}
+		@property --top {
+			syntax: '<length-percentage>';
+			initial-value: 0%;
+			inherits: false;
 		}
 
+		@property --right {
+			syntax: '<length-percentage>';
+			initial-value: 0%;
+			inherits: false;
+		}
+
+		@property --bottom {
+			syntax: '<length-percentage>';
+			initial-value: 0%;
+			inherits: false;
+		}
+
+		@property --left {
+			syntax: '<length-percentage>';
+			initial-value: 0%;
+			inherits: false;
+		}
 		header {
-			[data-section='header-background'] {
-				animation: header-scroll linear both;
-				animation-timeline: --hero-timeline;
-				animation-range: exit 0% 90%;
+			@keyframes scroll-progress {
+				from {
+					--right: 100%;
+				}
+				to {
+					--right: 0%;
+				}
+			}
+
+			[data-section='header-scroll-progress'] {
+				animation: scroll-progress linear;
+				animation-timeline: scroll();
+			}
+
+			:global {
+				@keyframes header-scroll {
+					from {
+						--bottom: -500%;
+					}
+					to {
+						--bottom: -200%;
+					}
+				}
+
+				+ main section:first-of-type {
+					&[data-section='hero'] {
+						view-timeline-name: --hero-timeline;
+					}
+				}
+
+				[data-section='header-background'] {
+					animation: header-scroll linear both;
+					animation-timeline: --hero-timeline;
+					animation-range: exit 0% 90%;
+				}
 			}
 		}
 	}

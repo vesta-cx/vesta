4235c9bee4d06c3587d340d0dd5187a9d3079d6c
mia-riezebos <mia@mia.cx>
2026-01-26 16:09:02 +0100

    export util components

--- src/lib/components/utils/index.ts
+++ src/lib/components/utils/index.ts
@@ -1,3 +1,4 @@
-import ViewTransition from './view-transition.svelte';
-
-export { ViewTransition };
+export * as Anchor from './anchor/index.js';
+export * as EasedGradient from './eased-gradient/index.js';
+export * as GradientBlur from './gradient-blur/index.js';
+export * as ViewTransition from './view-transition/index.js';

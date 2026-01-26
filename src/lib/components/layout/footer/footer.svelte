f6fc43825676abbb60c1e650a5160be52b97769c
mia-riezebos <mia@mia.cx>
2026-01-26 16:00:42 +0100

    move footer

---
+++ src/lib/components/layout/footer/footer.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	let { children }: { children: Snippet } = $props();
+</script>
+
+<footer>
+	{@render children()}
+</footer>
+
+<style>
+</style>

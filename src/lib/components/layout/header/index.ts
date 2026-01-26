36face77894f020b26fa120b620259ba1d60bcfd
mia-riezebos <mia@mia.cx>
2026-01-26 17:11:02 +0100

    export other header components

--- src/lib/components/layout/header/index.ts
+++ src/lib/components/layout/header/index.ts
@@ -1,9 +1,15 @@
 import Root from './header.svelte';
+import Logo from './header-logo.svelte';
+import Navigation from './header-navigation.svelte';
 
 export {
 	Root,
+	Logo,
+	Navigation,
 	//
-	Root as Header
+	Root as Header,
+	Logo as HeaderLogo,
+	Navigation as HeaderNavigation,
 };
 
 export default Root;

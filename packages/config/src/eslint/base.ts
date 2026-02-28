b35409e33c62c7f1454175dbec6b3c355d2fa877
mia-riezebos <mia@mia.cx>
2026-02-28 07:10:41 +0100

    fix rushstack incompat by flattening vercel style-guide into vesta-cx/config

--- packages/config/src/eslint/base.ts
+++ packages/config/src/eslint/base.ts
@@ -3,12 +3,11 @@
 import type { TSESLint } from "@typescript-eslint/utils";
 import eslintConfigPrettier from "eslint-config-prettier";
 import tseslint from "typescript-eslint";
-// @ts-ignore - @vercel/style-guide has no type definitions
-import vercel from "@vercel/style-guide/eslint/_base";
+import { vercelFlatCompat } from "./vercel-flat";
 
 export const base: TSESLint.FlatConfig.ConfigArray = [
 	...tseslint.configs.recommended,
-	vercel,
+	...vercelFlatCompat,
 	{
 		rules: {
 			//
